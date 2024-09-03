f683cd9257f54fe053576815d4ce9bc9656ab7e8
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2024-09-03 10:40:53 +0200

    Initial commit

---
+++ src/test/java/com/michelangelo/eurekaservermichelangelo/EurekaServerMichelangeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michelangelo.eurekaservermichelangelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerMichelangeloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
